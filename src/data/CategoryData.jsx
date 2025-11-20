8a2466020e87c23bf901be52b557368934635c5f
Sanju Kishor <sanjukishor17@gmail.com>
2025-11-20 20:25:16 +0530

    fix shop folder case issue

---
+++ src/data/CategoryData.jsx
@@ -0,0 +1,31 @@
+import P1 from "../../public/91n-oNCm3NL._AC_.jpg";
+import P2 from "../../public/101011ef36f2ed2e021c82505c6c4b5f.jpg";
+import P3 from "../../public/OIP.webp";
+import P4 from "../../public/OIP.webp"; 
+
+export const categoryData = [
+  {
+    id: 1,
+    name: "Festival Kits",
+    image: P1,
+    description: "Complete puja kits curated for every Hindu festival.",
+  },
+  {
+    id: 2,
+    name: "Daily Puja Items",
+    image: P2,
+    description: "Daily essentials for your everyday prayers.",
+  },
+  {
+    id: 3,
+    name: "Customized Kit",
+    image: P3,
+    description: "Create personalized ritual kits as per your needs.",
+  },
+  {
+    id: 4,
+    name: "Eco-friendly Products",
+    image: P4,
+    description: "Sustainable products crafted to respect nature.",
+  },
+];
